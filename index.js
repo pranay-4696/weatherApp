2245c00c20bda4c5d6f9ad0137934428affbe76a
pranay-4696 <pranaymacharla18@gmail.com>
2024-07-10 19:43:34 +0530

    css added

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express=require('express')
-const app=express()
-const https=require('https')
-const bodyParser=require('body-parser')
+const express = require('express');
+const app = express();
+const https = require('https');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",(req,res)=>{
-
     res.sendFile(__dirname+"/index.html");
 });
 
@@ -22,28 +23,51 @@ app.post("/",(req,res)=>{
       let  rand=Math.floor(Math.random() * cities.length)
         cityName=cities[rand]
     }
-    const url="https://api.openweathermap.org/data/2.5/weather?appid=2413aa948b867245ef6ba6759974ea96&q="+cityName+"&units=metric";
-    https.get(url,function(response){
-        response.on("data",(data)=>{
-            const weatherData=JSON.parse(data);
-            const temp=weatherData.main.temp;
-            const desc=weatherData.weather[0].description;
-            const icon=weatherData.weather[0].icon;
-            res.write("<h1>"+cityName+"</h1>")
-           
-            res.write("<p>The Temparature in "+cityName+":"+temp+"</p>")
-            res.write("<p>"+desc+"</p>")
-            const url=" http://openweathermap.org/img/wn/"+icon+"@2x.png";
-            res.write("<img src="+url+">");
-       // res.write();
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=2413aa948b867245ef6ba6759974ea96&q=" + cityName + "&units=metric";
+    https.get(url, function (response) {
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const result = `
+                <h1>${cityName}</h1>
+                <p>The Temperature in ${cityName}: ${temp}°C</p>
+                <p>${desc}</p>
+                <img src="${iconUrl}" alt="Weather Icon">
+            `;
+            res.send(`
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Weather App</title>
+                    <link rel="stylesheet" href="styles.css">
+                </head>
+                <body>
+                    <div class="container">
+                        <h1>Weather App</h1>
+                        <form action="/" method="post">
+                            <label for="cityName">Enter City Name:</label>
+                            <input type="text" id="cityName" name="cityName" placeholder="City Name">
+                            <button type="submit">Get Weather</button>
+                        </form>
+                        <div id="weatherResult">
+                            ${result}
+                        </div>
+                    </div>
+                </body>
+                </html>
+            `);
         });
-    })
+    });
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("running on port 3000");
-})
-
+});
 
 
 const cities = [
